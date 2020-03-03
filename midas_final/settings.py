be5bcc181a1f61c2a99b404170c19dde534425fb
Asifur Rahman <asifurarahman@gmail.com>
2020-03-03 08:52:10 +0600

    pneumonia and lung segmentation tensorflow

--- midas_final/settings.py
+++ midas_final/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '0d)@ni+&2!2q*2$gfal%fa6*i!j$0w6tunzg_6!=9hiwu_3-np'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
